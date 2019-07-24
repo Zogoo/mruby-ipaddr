3f756da4941f2c053bf7c208b9a86e19adfd3b22
takumin <takumiiinn@gmail.com>
2019-07-24 14:19:01 +0900

    support select mrb_int size

--- config.rb
+++ config.rb
@@ -3,4 +3,13 @@ MRuby::Build.new do |conf|
   enable_debug
   conf.enable_test
   conf.gem File.expand_path(File.dirname(__FILE__))
+
+  case ENV['MRB_INT_SIZE']
+  when 'MRB_INT16'
+    conf.cc.defines = %w(MRB_INT16)
+  when 'MRB_INT32'
+    conf.cc.defines = %w(MRB_INT32)
+  when 'MRB_INT64'
+    conf.cc.defines = %w(MRB_INT64)
+  end
 end
