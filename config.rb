765642f9abefd8414031b1e05e2cbe5c75a5c746
takumin <takumiiinn@gmail.com>
2019-07-24 16:26:46 +0900

    fix build config

--- config.rb
+++ config.rb
@@ -1,10 +1,14 @@
 MRuby::Build.new do |conf|
-  toolchain :gcc
-  enable_debug
-  conf.enable_test
-  conf.gem File.expand_path(File.dirname(__FILE__))
+  case ENV['MRUBY_TOOLCHAIN']
+  when 'vscpp'
+    toolchain :visualcpp
+  when 'clang'
+    toolchain :clang
+  else
+    toolchain :gcc
+  end
 
-  case ENV['MRB_INT_SIZE']
+  case ENV['MRUBY_INT_SIZE']
   when 'MRB_INT16'
     conf.cc.defines = %w(MRB_INT16)
   when 'MRB_INT32'
@@ -12,4 +16,8 @@ MRuby::Build.new do |conf|
   when 'MRB_INT64'
     conf.cc.defines = %w(MRB_INT64)
   end
+
+  enable_debug
+  conf.enable_test
+  conf.gem File.expand_path(File.dirname(__FILE__))
 end
